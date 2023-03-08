1ab32dbd329f2fe32a1f194f1e82508a5c6137ad
Juan Medina <jamedina@gmail.com>
2023-03-08 10:08:35 +0000

    adding draw game, working on #37

---
+++ examples/draw/src/draw_game.cpp
@@ -0,0 +1,56 @@
+/****************************************************************************
+MIT License
+
+Copyright (c) 2023 Juan Medina
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+****************************************************************************/
+
+#include "draw_game.hpp"
+
+draw_game::draw_game(): application("sneze", "Draw Game") {}
+
+namespace logger = sneze::logger;
+using config = sneze::config;
+namespace components = sneze::components;
+using color = components::color;
+
+auto draw_game::configure() -> config {
+    logger::debug("configure");
+
+    namespace keyboard = sneze::keyboard;
+    using key = keyboard::key;
+    using modifier = keyboard::modifier;
+
+    return config()
+        .size(1920, 1080)
+        .clear(color::light_gray)
+        .exit(key::escape)
+        .toggle_full_screen(modifier::alt, key::_return);
+}
+
+auto draw_game::init() -> sneze::result<> {
+    logger::debug("init draw game");
+
+    return true;
+}
+
+void draw_game::end() {
+    logger::debug("ending draw game");
+}
