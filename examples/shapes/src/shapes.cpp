bbf18ba67a61c78045a9bad85dd7951da228e781
Juan Medina <jamedina@gmail.com>
2023-03-10 07:58:55 +0000

    fixing inspections, working on #38

--- examples/shapes/src/shapes.cpp
+++ examples/shapes/src/shapes.cpp
@@ -26,7 +26,7 @@ SOFTWARE.
 
 #include "shapes_game.hpp"
 
-auto main(int, char *[]) -> int {
+auto main(int /*argc*/, char * /*argv*/[]) -> int {
     if(auto err = shapes_game().run().ko(); err) {
         return EXIT_FAILURE;
     }
