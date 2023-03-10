59f8d0fb74edc38e343caab259a8d7a5ad8b4a82
Juan Medina <jamedina@gmail.com>
2023-03-10 09:32:30 +0000

    add list headers for linux

--- lib/list_headers.sh
+++ lib/list_headers.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-find include -name "*.hpp" | sort | sed -r 's/include\/sneze\//\#include "/' | sed -r 's/\.hpp/\.hpp"/' | grep -v 'sneze.hpp'ch | xclip -selection clipboard
+find include -name "*.hpp" | sort | sed -r 's/include\/sneze\//\#include "/' | sed -r 's/\.hpp/\.hpp"/' | grep -v 'sneze.hpp' | xclip -selection clipboard
