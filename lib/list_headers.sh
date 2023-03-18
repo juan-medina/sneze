5bc3cd4d9e17ac9ea7b948cfa0b3b4f4a01d4c8e
Juan Medina <jamedina@gmail.com>
2023-03-18 06:30:36 +0000

    update headers, working on #42

--- lib/list_headers.sh
+++ lib/list_headers.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-find include -name "*.hpp" | sort | sed -r 's/include\/sneze\//\#include "/' | sed -r 's/\.hpp/\.hpp"/' | grep -v 'sneze.hpp' | xclip -selection clipboard
+if [[ $OSTYPE == 'darwin'* ]]; then
+  COPY_CLIP='pbcopy'
+else
+  COPY_CLIP='xclip -selection clipboard'
+fi
+
+find include -name "*.hpp" | sort | sed -r 's/include\/sneze\//\#include "/' | sed -r 's/\.hpp/\.hpp"/' | grep -v 'sneze.hpp' | $COPY_CLIP
+
+echo "Headers copied to clipboard"
