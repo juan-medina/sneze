0d10488dd770d9c8efc9b268d9cd724693a0148a
Juan Medina <jamedina@gmail.com>
2023-03-24 10:53:55 +0000

    adding system doc, working on juan-medina/sneze#54

--- lib/include/sneze/systems/system.hpp
+++ lib/include/sneze/systems/system.hpp
@@ -30,9 +30,18 @@ namespace sneze {
 
 class world;
 
+/**
+ * @brief base class for all systems
+ * @see sneze::world
+ * @see sneze::world::add_system
+ * @see sneze::world::add_system_with_priority
+ */
 struct system {
+    //! initialize the system
     virtual void init(world *world) = 0;
+    //! update the system
     virtual void update(world *world) = 0;
+    //! shutdown the system
     virtual void end(world *world) = 0;
 
     system() = default;
@@ -45,32 +54,58 @@ struct system {
     auto operator=(const system &&) -> system & = delete;
 };
 
+/**
+ * @brief system with priority
+ * @see sneze::world::add_system_with_priority
+ */
 class system_with_priority {
 public:
+    /**
+     * @brief Construct a new system with priority object
+     * @param type the type id of the system
+     * @param priority the priority of the system
+     * @param system the system
+     */
     system_with_priority(const entt::id_type type, std::int32_t priority, std::unique_ptr<system> system)
         : type_{type}, system_{std::move(system)}, priority_{priority} {}
 
+    //! initialize the system
     void init(world *world) {
         system_->init(world);
     }
+
+    //! update the system
     void update(world *world) {
         system_->update(world);
     }
+
+    //! shutdown the system
     void end(world *world) {
         system_->end(world);
     }
 
+    /**
+     * @brief Get the priority of the system
+     * @return the priority of the system
+     */
     [[nodiscard]] inline auto priority() const -> auto & {
         return priority_;
     }
 
+    /**
+     * @brief Get the type of the system
+     * @return the type of the system
+     */
     [[nodiscard]] inline auto type() const -> auto & {
         return type_;
     }
 
 private:
+    //! the type id of the system
     entt::id_type type_{};
+    //! the system
     std::unique_ptr<system> system_{};
+    //! the priority of the system
     std::int32_t priority_{};
 };
 
